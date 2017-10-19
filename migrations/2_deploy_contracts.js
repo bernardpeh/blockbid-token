5cfaa71b3d10ba0cfaf09d6b11a63a503ec4fef1
bernard peh <bpeh777@gmail.com>
2017-10-19 20:47:33 +1100

    add wallet address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,7 +4,8 @@ var startTime = 1508590800;
 var endTime = startTime + (604800 * 8)
 var rate = 300;
 var earlybonus = 60;
-var etherWallet = web3.eth.accounts[1];
+var etherWallet = "0xFC33a201BB96457F14D19b6752f0bEBAEbDE6E1d";
+// var etherWallet = web3.eth.accounts[1];
 var cap = 3000000000000000;
 var goal = 100000000000000;
 
@@ -14,4 +15,3 @@ module.exports = function(deployer) {
     deployer.deploy(BlockbidCrowdsale, goal, cap, startTime, endTime, rate, earlybonus, etherWallet);
 };
 
-// 100000000000000,3000000000000000,1507986672,1507992672,300,60,"0xca35b7d915458ef540ade6068dfe2f44e8fa733c"
